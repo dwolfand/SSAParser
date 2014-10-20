7c381b0bc38453cd0cfccb2237b7153a33fef229
Johnny Reading <jreading@hellowallet.com>
2014-10-20 18:07:51 -0400

    clear table when loading

--- app/scripts/parser.js
+++ app/scripts/parser.js
@@ -34,28 +34,34 @@ function parseEarnings(textArray, index){
 
 function printEarnings(earnings){
     $("#output").empty();
+    var output = $("<table id=output-table></table>").addClass("table table-striped display");
+    var head = $("<thead></thead>");
+    var tbody = $("<tbody></tbody>");
+    $("#output").append(output);
+    head.append("<tr><th>Year</th><th>Earnings</th><th>Inflated</th><th>Inflation Percent</th><th>Inflation Calc</th></tr>");
+    output.append(head).end().append(tbody);
+    
     for (var i = 0; i < earnings.length; i++) {
-        var output = $("<tr></tr>");
+        var row = $("<tr></tr>");
+        tbody.append(row);
         if (earnings[i].isTopYear){
-          output = $('<tr class="top-year"></tr>');
+          row.addClass('top-year');
         }
-        output.append($("<td></td>").text(earnings[i].year));
-        output.append($("<td></td>").text(earnings[i].ssEarnings));
-        output.append($("<td></td>").text(earnings[i].inflatedEarnings));
-        output.append($("<td></td>").text(earnings[i].inflationPercent));
-        output.append($("<td></td>").text(earnings[i].inflationCalc));
-        $("#output").append(output);
+        row.append($("<td></td>").text(earnings[i].year));
+        row.append($("<td></td>").text(earnings[i].ssEarnings));
+        row.append($("<td></td>").text(earnings[i].inflatedEarnings));
+        row.append($("<td></td>").text(earnings[i].inflationPercent));
+        row.append($("<td></td>").text(earnings[i].inflationCalc));
+        output.append(tbody);
     }
-
-    $("table").removeClass("hide");
-    $("table").dataTable({
-        "retrieve": true,
+    $("#output-table").dataTable({
         "paging":   false,
         "ordering": true,
         "order": [[ 2, "desc" ]],
         "info":     false,
-        "search":   false
+        "searching":   false
     });
+
 }
 
 function inflateEarnings(earnings){
