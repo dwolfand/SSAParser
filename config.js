580f2b0c682255c4b4c17cf9babc4218800037ab
David Wolfand <david@hellowallet.com>
2014-10-20 12:58:51 -0400

    adding brunch for build process

---
+++ config.js
@@ -0,0 +1,34 @@
+exports.config = {
+  paths: {
+    watched: ['app', 'vendor']
+  },
+  files: {
+    javascripts: {
+      joinTo: {
+        'scripts/app.js': /^(app)/,
+        'scripts/vendor.js': /^(vendor(\/||\\)scripts)/
+      },
+      order: {
+        before: [
+          'vendor/scripts/bignumber.js',
+          'vendor/scripts/pdf.js',
+          'vendor/scripts/pdf.worker.js'
+        ]
+      }
+    },
+    stylesheets: {
+      joinTo: {
+        'stylesheets/app.css': /^(app|vendor(\/||\\)styles)/
+      }
+    },
+    templates: {
+      precompile: true,
+      root: 'templates',
+      joinTo: {
+        'javascripts/app.js': /^app/
+      }
+    }
+  },
+  overrides: {
+  }
+};
