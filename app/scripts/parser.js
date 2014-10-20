c0c41ec397d310aa0c915f9b719ee8b4913b3eee
David Wolfand <david@Davids-MacBook-Pro-2.local>
2014-10-20 16:06:31 -0400

    updated with correct inflation calculations

--- app/scripts/parser.js
+++ app/scripts/parser.js
@@ -1,7 +1,7 @@
-var indexingFactors = require("scripts/indexingFactors");
-var age = 28;
-var salary = new BigNumber("100000");
-var currency = require("scripts/currency");
+var indexingFactors = require("scripts/indexingFactors"),
+    currency = require("scripts/currency"),
+    age = 60,
+    salary = new currency(100000);
 
 PDFJS.disableWorker = true;
 // console.log("blah",document.getElementsByTagName('head')[0]);
@@ -37,88 +37,121 @@ function printEarnings(earnings){
     var output = $("<tr></tr>");
     output.append($("<td></td>").text(earnings[i].year));
     output.append($("<td></td>").text(earnings[i].ssEarnings));
-    output.append($("<td></td>").text(earnings[i].medEarnings));
+    output.append($("<td></td>").text(earnings[i].inflatedEarnings));
+    output.append($("<td></td>").text(earnings[i].inflationPercent));
+    output.append($("<td></td>").text(earnings[i].inflationCalc));
     $("#output").append(output);
     $("table").removeClass("hide");
   }
 }
 
+function inflateEarnings(earnings){
+  var baseWageIndexYear = 2014;
+  if (age > 60){
+    baseWageIndexYear = 2014 - (age-60);
+  }
+  var baseWageIndexAmt = indexingFactors[baseWageIndexYear];
+  console.log("amount", indexingFactors, baseWageIndexYear, baseWageIndexAmt.toString());
+  for (var i = 0; i < earnings.length; i++) {
+    var curYearEarn = earnings[i];
+    if (curYearEarn.year >= 2014 || curYearEarn.year >= baseWageIndexYear){
+      curYearEarn.inflationPercent = 1;
+      curYearEarn.inflatedEarnings = curYearEarn.ssEarnings;
+    } else {
+      curYearEarn.inflationCalc = baseWageIndexAmt + "("+ baseWageIndexYear +") / " + indexingFactors[curYearEarn.year] + "(" + curYearEarn.year + ")";
+      curYearEarn.inflationPercent = new BigNumber(baseWageIndexAmt.div(indexingFactors[curYearEarn.year]));
+      curYearEarn.inflatedEarnings = curYearEarn.ssEarnings.times(curYearEarn.inflationPercent);
+    }
+  }
+}
+
 function sortEarnings(earnings){
   return earnings.sort(function(a, b){
-    return b.ssEarnings.cmp(a.ssEarnings);
+    return b.inflatedEarnings.cmp(a.inflatedEarnings);
   });
 }
 
 function projectEarnings(earnings){
-  earnings.push({
-    year: 2015,
-    ssEarnings: new currency("117000"),
-    medEarnings: new currency("117000")
-  });
+  //for every year up to age 65 add cur salary
+  for (var i = age; i < 65; i++) {
+    earnings.push({
+      year: 2014 + (i-age),
+      ssEarnings: salary,
+      medEarnings: salary
+    });
+  }
   return earnings;
 }
 
 function handleFileSelect(evt) {
-  var files = evt.target.files; // FileList object
-
-  // Loop through the FileList and render image files as thumbnails.
-  for (var i = 0, f; f = files[i]; i++) {
-
+  try {
+    var files = evt.target.files; // FileList object
     var reader = new FileReader();
 
     // Closure to capture the file information.
     reader.onload = (function(theFile) {
-      return function(e) {
-        PDFJS.getDocument(e.target.result).then(function getPdfHelloWorld(pdf) {
-          console.log("test",pdf.get);
-          
-          //
-          // Fetch the first page
-          //
-          pdf.getPage(3).then(function getPageHelloWorld(page) {
-            var scale = 1.5;
-            var viewport = page.getViewport(scale);
+      try {
+        return function(e) {
+          PDFJS.getDocument(e.target.result).then(function getPdfHelloWorld(pdf) {
+            //
+            // Fetch the third page since thats where earning are
+            //
+            pdf.getPage(3).then(function getPageHelloWorld(page) {
+              var scale = 1.5;
+              var viewport = page.getViewport(scale);
 
-            page.getTextContent().then( function(textContent){
-              console.log(textContent);
-              var earnings = [];
-              for (var j = 0; j <= textContent.items.length; j++) {
-                var item = textContent.items[j];
-                if (item.str === "Medicare" && textContent.items[j+1].str === "Earnings"){
-                  console.log("found it!", indexingFactors);
-                  earnings = parseEarnings(textContent.items, j+2);
-                  projectEarnings(earnings);
-                  sortEarnings(earnings);
-                  console.log("earnings", earnings);
-                  printEarnings(earnings);
-                  break;
+              page.getTextContent().then( function(textContent){
+                console.log(textContent);
+                var earnings = [];
+                for (var j = 0; j <= textContent.items.length; j++) {
+                  var item = textContent.items[j];
+                  if (item.str === "Medicare" && textContent.items[j+1].str === "Earnings"){
+                      try {
+                        console.log("found it!", indexingFactors);
+                        earnings = parseEarnings(textContent.items, j+2);
+                        projectEarnings(earnings);
+                        inflateEarnings(earnings);
+                        sortEarnings(earnings);
+                        console.log("earnings", earnings);
+                        printEarnings(earnings);
+                      }
+                      catch(err){
+                        console.error("Error on parse: ",err);
+                      }
+                    break;
+                  }
                 }
-              }
+              });
+
+              //
+              // Prepare canvas using PDF page dimensions
+              //
+              var canvas = document.getElementById('the-canvas');
+              var context = canvas.getContext('2d');
+              canvas.height = viewport.height;
+              canvas.width = viewport.width;
+
+              //
+              // Render PDF page into canvas context
+              //
+              var renderContext = {
+                canvasContext: context,
+                viewport: viewport
+              };
+              page.render(renderContext);
             });
-
-            //
-            // Prepare canvas using PDF page dimensions
-            //
-            var canvas = document.getElementById('the-canvas');
-            var context = canvas.getContext('2d');
-            canvas.height = viewport.height;
-            canvas.width = viewport.width;
-
-            //
-            // Render PDF page into canvas context
-            //
-            var renderContext = {
-              canvasContext: context,
-              viewport: viewport
-            };
-            page.render(renderContext);
           });
-        });
-      };
-    })(f);
+        };
+      }
+      catch(err){
+        console.error("Error on load: ",err);
+      }
+    })(files[0]);
 
-    // Read in the image file as a data URL.
-    reader.readAsArrayBuffer(f);
+    reader.readAsArrayBuffer(files[0]);
+  }
+  catch(err){
+    console.log("Error: ",err);
   }
 }
 
