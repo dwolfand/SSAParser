d8134957dad823e7cd013563d6de4b12482eaa89
David Wolfand <david@hellowallet.com>
2014-10-21 10:19:38 -0400

    pulling out the print function into its own file and adding a top year row style

--- app/scripts/parser.js
+++ app/scripts/parser.js
@@ -1,5 +1,6 @@
 var indexingFactors = require("scripts/indexingFactors");
 var currency = require("scripts/currency");
+var printEarnings = require("scripts/printEarnings");
 var age = 28;
 var salary = new currency("100000");
 
@@ -30,38 +31,6 @@ function parseEarnings(textArray, index){
     }
   }
   return earnings;
-}
-
-function printEarnings(earnings){
-    $("#output").empty();
-    var output = $("<table id=output-table></table>").addClass("table table-striped display");
-    var head = $("<thead></thead>");
-    var tbody = $("<tbody></tbody>");
-    $("#output").append(output);
-    head.append("<tr><th>Year</th><th>Earnings</th><th>Inflated</th><th>Inflation Percent</th><th>Inflation Calc</th></tr>");
-    output.append(head).end().append(tbody);
-    
-    for (var i = 0; i < earnings.length; i++) {
-        var row = $("<tr></tr>");
-        tbody.append(row);
-        if (earnings[i].isTopYear){
-          row.addClass('top-year');
-        }
-        row.append($("<td></td>").text(earnings[i].year));
-        row.append($("<td></td>").text(earnings[i].ssEarnings));
-        row.append($("<td></td>").text(earnings[i].inflatedEarnings));
-        row.append($("<td></td>").text(earnings[i].inflationPercent));
-        row.append($("<td></td>").text(earnings[i].inflationCalc));
-        output.append(tbody);
-    }
-    $("#output-table").dataTable({
-        "paging":   false,
-        "ordering": true,
-        "order": [[ 2, "desc" ]],
-        "info":     false,
-        "searching":   false
-    });
-
 }
 
 function inflateEarnings(earnings){
